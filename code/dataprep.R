b5bea231519aacdff9e7f388aa0a08d68c2ac08d
Janik Hoffmann <janikhoffmann@janiks-mbp.speedport.ip>
2023-04-17 08:59:44 +0200

    Set up data code

---
+++ code/dataprep.R
@@ -0,0 +1,53 @@
+### data preparation ###
+
+# load required libraries
+library(sf)
+library(raster)
+library(terra)
+library(rgdal)
+library(gdalUtils)
+
+
+
+
+
+# plot AOI outlines: Hohe Tauern Nationalpark
+
+
+
+
+# load annual summer Landsat data (B2, B3, B4, B6)
+
+
+
+
+  # 2000
+
+  # 2005
+
+  # 2010
+
+  # 2015
+
+  # 2020
+
+
+
+# image classification: mapping of ice surfaces
+
+
+
+
+
+
+
+# load SRTM DEM data
+
+
+
+
+# terrain analysis
+
+
+
+
