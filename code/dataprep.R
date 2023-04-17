5f712257ab4eb111c46230118ea2798488bb0fc1
Janik Hoffmann <janikhoffmann@janiks-mbp.speedport.ip>
2023-04-17 09:05:11 +0200

    load aoi

--- code/dataprep.R
+++ code/dataprep.R
@@ -13,8 +13,9 @@ library(gdalUtils)
 
 # plot AOI outlines: Hohe Tauern Nationalpark
 
-
-
+setwd("/Volumes/satdata/uni_wuerzburg/Programming/topoglacier_data")
+aoi <- st_read("npht_agrenze_wgs84.shp")
+plot(aoi)
 
 # load annual summer Landsat data (B2, B3, B4, B6)
 
