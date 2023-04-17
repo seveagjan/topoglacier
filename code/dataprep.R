e542daa00e462a2d0cc2a6b94052f2bff0cc8568
Janik Hoffmann <janikhoffmann@janiks-mbp.speedport.ip>
2023-04-17 13:30:05 +0200

    finished ndsi computation

--- code/dataprep.R
+++ code/dataprep.R
@@ -1,6 +1,6 @@
 ### data preparation ###
 
-# load required libraries
+### load required libraries
 library(sf)
 library(raster)
 library(terra)
@@ -11,13 +11,13 @@ library(gdalUtils)
 
 
 
-# plot AOI outlines: Hohe Tauern Nationalpark
+### plot AOI outlines: Hohe Tauern Nationalpark
 
 setwd("/Volumes/satdata/uni_wuerzburg/Programming/topoglacier_data")
 aoi <- st_read("npht_agrenze_wgs84.shp")
 plot(aoi[1])
 
-# load summer Landsat data from 2013 and 2022 as multi-band images (B2, B3, B4, B6)
+### load summer Landsat data from 2013 and 2022 as multi-band images (B2, B3, B4, B6)
 
   # 2013
 
@@ -42,20 +42,66 @@ plotRGB(l8_2013_aoi, r=3, g=2, b=1, scale= 50000)
 l8_2022_aoi <- terra::trim(mask(lc08_2022, aoi))
 plotRGB(l8_2022_aoi, r=3, g=2, b=1, scale= 50000)
 
-# image classification: mapping of ice surfaces
+### mapping of ice surfaces
+
+# calculate the Normalized Difference Snow Index (NDSI)
+
+landsat_ndsi <- function(x,y) {(x-y)/(x+y)}
+
+l8_2013_aoi_ndsi <- landsat_ndsi(l8_2013_aoi$LC08_L2SP_192027_20130803_20200912_02_T1_SR_B3,
+                                 l8_2013_aoi$LC08_L2SP_192027_20130803_20200912_02_T1_SR_B6)
+names(l8_2013_aoi_ndsi) <- "NDSI"
+l8_2022_aoi_ndsi <- landsat_ndsi(l8_2022_aoi$LC09_L2SP_192027_20220719_20230407_02_T1_SR_B3,
+                                 l8_2022_aoi$LC09_L2SP_192027_20220719_20230407_02_T1_SR_B6)
+names(l8_2022_aoi_ndsi) <- "NDSI"
+plot(l8_2013_aoi_ndsi)
+plot(l8_2022_aoi_ndsi)
+
+## extract ice surfaces by applying a NDSI threshold to each image
+
+# define NDSI threshold by calculating average NDSI within regional RGI glacier outlines
+
+setwd("/Volumes/satdata/uni_wuerzburg/Programming/topoglacier_data")
+rgi_npht_glacier <- st_read("rgi_npht.shp")
+rgi_npht_glacier <- st_transform(rgi_npht_glacier, crs = crs(lc08_2013))
+
+npht_glacier_ndsi2013 <- terra::extract(l8_2013_aoi_ndsi, rgi_npht_glacier, fun = mean, na.rm = TRUE, df = TRUE)
+mean(npht_glacier_ndsi2013$NDSI) # threshold of 0.4 is chosen, comparable to literature (see https://doi.org/10.5194/tc-12-1629-2018)
+
+ndsithreshold <- function(x) {
+  m <-  c(-1, 0.4, 0,
+          0.4, 1, 1)
+  
+  rmat <- matrix(m, ncol=3, byrow=TRUE)
+  
+  icesurface_mask <- terra::classify(x, rmat)
+}
+
+surfaceice2013_mask <- ndsithreshold(l8_2013_aoi_ndsi)
+names(surfaceice2013_mask) <- "binary_icecover"
+plot(surfaceice2013_mask)
+
+surfaceice2022_mask <- ndsithreshold(l8_2022_aoi_ndsi)
+names(surfaceice2022_mask) <- "binary_icecover"
+plot(surfaceice2022_mask)
+
+## validate surface ice mask with RGI data on glacier extent
+
+# validation area value
+
+sum(rgi_npht_glacier$AREA) # unit = sqkm 
+
+# NDSI-based area value
+
+(sum(values(surfaceice2013_mask$binary_icecover == 1), na.rm = TRUE)*900) / 1000000
+
+
+### load SRTM DEM data
 
 
 
 
-
-
-
-# load SRTM DEM data
-
-
-
-
-# terrain analysis
+### terrain analysis
 
 
 
