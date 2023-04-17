0f73b0abab0be728a6375f4e9f838ad5a7cdfc60
Janik Hoffmann <janikhoffmann@janiks-mbp.speedport.ip>
2023-04-17 16:06:36 +0200

    ice loss mapping

--- code/analysis.R
+++ code/analysis.R
@@ -12,3 +12,31 @@
 
 
 ### load required packages
+
+
+
+### change detection - mapping of areal ice loss
+
+iceloss <- surfaceice2022_mask - surfaceice2013_mask
+iceloss[iceloss == 1] <- 0
+iceloss[iceloss == -1] <- 1
+plot(iceloss)
+
+# quantify ice loss in sqkm
+
+(sum(values(iceloss == 1), na.rm = TRUE)*900) / 1000000
+
+# vectorize areas of ice loss
+
+iceloss[iceloss == 0] <- NA
+iceloss_vector <- terra::as.polygons(iceloss, dissolve = TRUE)
+plot(iceloss_vector)
+
+
+
+
+
+
+
+
+
